57eb11ed9f114236d6dfafc3ce7bc49c700cf9d1
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-10-05 12:14:09 -0700

    Add toggle for google analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import { LitElement, html } from 'lit-element';
 import Tabs from './components/tabs';
 
-//firebase.analytics();
-// legal requirements not met
+if (localStorage.getItem('consent') === 'true') {
+  firebase.analytics();
+} else if (localStorage.getItem('consent') === 'false') {
+  console.log('No Firebase Analytics');
+} else {
+  const cookieDisclaimer = document.querySelector('.cookies');
+  cookieDisclaimer.style.display = 'block';
+
+  cookieDisclaimer.querySelector('button').addEventListener('click', () => {
+    cookieDisclaimer.style.display = 'none';
+    localStorage.setItem('consent', 'true');
+    firebase.analytics();
+  })
+
+  cookieDisclaimer.querySelector('img').addEventListener('click', event => {
+    event.preventDefault();
+    cookieDisclaimer.style.display = 'none';
+
+    if (confirm('Disable Usage Tracking?')) {
+      console.log('No Firebase Analytics');
+    } else {
+      localStorage.setItem('consent', true);
+      firebase.analytics();
+    }
+  })
+}
